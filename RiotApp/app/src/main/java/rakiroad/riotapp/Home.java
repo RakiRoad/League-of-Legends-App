93dc4ec616dea73f7d2796c173b2714870db4107
RakiRoad <quintor2@tcnj.edu>
2017-05-31 20:23:31 -0400

    Made Changes to the UI
    Redesigned the UI. Current incomplete and is a work in progress

--- RiotApp/app/src/main/java/rakiroad/riotapp/Home.java
+++ RiotApp/app/src/main/java/rakiroad/riotapp/Home.java
@@ -1,13 +1,46 @@
 package rakiroad.riotapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.view.View;
+import android.net.Uri;
 
 public class Home extends AppCompatActivity {
-
+    String selected, spinner_item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Spinner spinner_region = (Spinner) findViewById(R.id.selectRegion);
+        ArrayAdapter<String> adapter_region = new ArrayAdapter<String>(Home.this, android.R.layout.simple_expandable_list_item_1
+            , getResources().getStringArray(R.array.Regions));
+        adapter_region.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_region.setAdapter(adapter_region);
+
+        ButtonDefinition();
     }
+
+    public void ButtonDefinition() {
+        findViewById(R.id.btnSearch).setOnClickListener(buttonClickListener);
+    }
+
+    private View.OnClickListener buttonClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            switch (view.getId()){
+                case R.id.btnSearch:
+                    Intent intent = new Intent(Home.this, MMR_Results.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+
+
+
+
 }
---
+++ RiotApp/app/src/main/java/rakiroad/riotapp/MMR_Results.java
@@ -0,0 +1,8 @@
+package rakiroad.riotapp;
+
+/**
+ * Created by Rocky on 5/31/2017.
+ */
+
+public class MMR_Results {
+}
