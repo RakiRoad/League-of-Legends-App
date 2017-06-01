f1baa339ea42b286bdd569345907bf48915cf88a
RakiRoad <quintor2@tcnj.edu>
2017-05-31 23:36:43 -0400

    Background change
    Made some more UI changes

--- RiotApp/app/src/main/java/rakiroad/riotapp/MMR_Results.java
+++ RiotApp/app/src/main/java/rakiroad/riotapp/MMR_Results.java
@@ -3,6 +3,17 @@ package rakiroad.riotapp;
 /**
  * Created by Rocky on 5/31/2017.
  */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
 
-public class MMR_Results {
+public class MMR_Results extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results_page);
+        //Toast.makeText(this, "Why do they fear me?", Toast.LENGTH_LONG).show();
+    }
 }
